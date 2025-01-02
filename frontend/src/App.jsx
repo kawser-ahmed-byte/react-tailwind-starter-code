e81a46bcde9fb45e230045f77df298a3d7a6d20e
kawser <kawserahmed2001@gmail.com>
2025-01-02 21:05:34 +0600

    Initial commit: React + Tailwind starter code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+    return (
+        <>
+            <h1 className='text-3xl text-red-600'>Starter Code for tailwind and react</h1>
+        </>
+    )
+}
+
+export default App
